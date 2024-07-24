3aa27f3cf2113c9e885cd7f0a3f5e0110b555cd0
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 15:27:10 +0000

    getFullBudgetObject to use ES6 method properties in the fullBudget object

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,20 @@
+/*
+ * Run "npm run dev maintest/8-main.js" to test
+*/
+
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
