cbffad020cc2dedffd311802a1396ee7c330edab
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 07:18:00 +0000

    convert standard to anonymous function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,15 @@
+/*
+ * Run "npm run dev maintest/2-main.js" to test
+*/
+
+/* eslint-disable no-unused-vars */
+
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
