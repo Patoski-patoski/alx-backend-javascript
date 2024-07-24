b846df9201d452d7d0d2a850085e481f80c8d3bf
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 20:09:01 +0000

    a function named createEmployeesObject that will receive two arguments

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,9 +1,9 @@
 /*
  * Run "npm run dev maintest/11-main.js" to test
 */
-export default function createEmployeesObjecg(departmentName, employees) {
+export default function createEmployeesObject(departmentName, employees) {
   const employeesObject = {
-    [departmentName]: [...employees]
+    [departmentName]: [...employees],
   };
 
   return employeesObject;
