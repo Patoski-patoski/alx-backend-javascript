baeeeb7a03d1a207feec6f544f8bc1f4cbdd0cd1
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 14:40:59 +0000

    function to use ES6 computed property names on the budget object

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,20 @@
+/*
+ * Run "npm run dev maintest/7-main.js" to test
+*/
+
+function getCurrentYear () {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear (income, gdp, capita) {
+  const year = getCurrentYear();
+
+  const budget = {
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita
+  };
+
+  return budget;
+}
