51a66a9cd87d73a600a37637797fade5a2f0b114
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 16:35:02 +0000

    Rewrite the function appendToEachArrayValue to use ES6’s for...of operator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 /*
  * Run "npm run dev maintest/10-main.js" to test
 */
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (const value of array) {
     newArray.push(appendString + value);
