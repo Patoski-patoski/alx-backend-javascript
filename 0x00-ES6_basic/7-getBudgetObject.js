a21112550ac89a166505ac2868209d04df106bb2
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 14:29:39 +0000

    Modify the function’s budget object to simply use the keyname instead.

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -3,7 +3,11 @@
 */
 
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
 }
