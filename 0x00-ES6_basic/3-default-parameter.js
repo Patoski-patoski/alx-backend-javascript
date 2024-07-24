fe033c7fb66397da65c39a8296c4be413e9e5c00
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 07:26:43 +0000

    define default parameter values for the function parameters.

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,9 @@
+/*
+ * Run "npm run dev maintest/3-main.js" to test
+*/
+
+/* eslint-disable no-unused-vars */
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
