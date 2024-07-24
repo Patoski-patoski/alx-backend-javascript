e9dce0f7a082e1f3a9cb9d55c1a4b96f2a44731c
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-24 05:22:55 +0000

    modify codes to use let and const

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -6,18 +6,18 @@
  * Return nothing
  **/
 
-export function taskFirst () {
-  const task = 'I prefer const when I can.';
-  return task;
+export function taskFirst() {
+    const task = 'I prefer const when I can.';
+    return task;
 }
 
-export function getLast () {
-  return ' is okay';
+export function getLast() {
+    return ' is okay';
 }
 
-export function taskNext () {
-  let combination = 'But sometimes let';
-  combination += getLast();
-  
-  return combination;
+export function taskNext() {
+    let combination = 'But sometimes let';
+    combination += getLast();
+
+    return combination;
 }
