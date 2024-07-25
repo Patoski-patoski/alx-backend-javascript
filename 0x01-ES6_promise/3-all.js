0dc8d779b5d4d70a2c6e98ea49291dbe3f1a796c
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-25 16:25:35 +0000

    resolve all promises and log body firstName lastName to the console

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ import { uploadPhoto, createUser } from './utils';
  * Run "npm run dev testFolder/3-main.js" to test
 */
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
