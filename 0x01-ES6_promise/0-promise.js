ecc3dc8a0ba6241aeb791eed4f9b020030b84d85
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-25 04:57:40 +0000

    return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,10 @@
 /*
- * Run "npm run devtestFolder/0-main.js" to test
+ * Run "npm run dev testFolder/0-main.js" to test
 */
+
 /* eslint-disable ng-unused-vars */
 
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
   return new Promise(() => {
   });
 }
