89680448673341827a1f65fe2a023bfc97b257ce
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-25 23:16:21 +0000

    function should call the two other functions. it should return an array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ import uploadPhoto from './5-photo-reject';
  * Run "npm run dev testFolder/5-main.js" to test
 */
 
-export default async function handleProfileSignup (firstName, lastName, filename) {
+export default async function handleProfileSignup(firstName, lastName, filename) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename)
