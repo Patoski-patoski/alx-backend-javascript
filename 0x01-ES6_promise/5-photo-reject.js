4059401e15cb7175401f1bb496551afd4b374de2
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-25 20:47:12 +0000

    return a Promise rejecting with an Error and the string

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+/*
+ * Run "npm run dev testFolder/5-main.js" to test
+*/
+
+export default function uploadPhoto (filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
