ef0f35335a6a1dddc3eb2b6dc767b2823bc2e0db
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-25 23:15:26 +0000

    function should call the two other functions. it should return an array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,19 +5,14 @@ import uploadPhoto from './5-photo-reject';
  * Run "npm run dev testFolder/5-main.js" to test
 */
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-  try {
-    const userPromise = await signUpUser(firstName, lastName);
-    const photoPromise = await uploadPhoto(filename);
-    const [userResult, photoResult] = await Promise.all(([userPromise, photoPromise]));
+export default async function handleProfileSignup (firstName, lastName, filename) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename)
+  ]);
 
-    return [
-      { status: userResult.status, value: userResult.value },
-      { status: photoResult.status, value: photoResult.value }
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error }
-    ];
-  }
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason)
+  }));
 }
