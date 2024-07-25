b79d2443ae9e84be286f858469c7b31ce849e513
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-25 18:00:51 +0000

    returns a resolved promise with this object:

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,10 @@
+/*
+ * Run "npm run dev testFolder/4-main.js" to test
+*/
+
+export default function signUpUser (firstName, lastName) {
+  return Promise.resolve({
+    firstName,
+    lastName
+  });
+}
