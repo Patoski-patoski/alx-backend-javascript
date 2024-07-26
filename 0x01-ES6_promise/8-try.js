91768dd4741f2ff2edd80272bbc1a871f48e97c0
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-26 23:27:41 +0000

    function named divideFunction that will accept two arguments: numerator and denominator

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+/*
+ * Run "npm run dev testFolder/8-main.js" to test
+*/
+export default function divideFunction (numerator, denominator) {
+  if (denominator === 0) throw new Error('cannot divide by 0');
+  return Number(numerator / denominator);
+}
