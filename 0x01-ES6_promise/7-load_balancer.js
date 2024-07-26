702e8b61b874d710f7e2b71251967d512b4ed1e6
Okafor Patrick C <patrickpeters911@gmail.com>
2024-07-26 23:04:01 +0000

    function should return the value returned by the promise that resolved the first.

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,6 @@
 /*
  * Run "npm run dev testFolder/7-main.js" to test
 */
-export default async function loadBalancer (chinaDownload, USDownload) {
+export default async function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload]);
 }
