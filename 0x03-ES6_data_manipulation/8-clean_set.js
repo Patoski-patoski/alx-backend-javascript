a0b0e7e72d5e301a86c93f1581c8a100229e17a5
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-01 06:05:34 +0000

    returns a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString === undefined || typeof (startString) !== 'string' || startString === '') return '';
-  const newString = [...set]
-    .filter((item) => item.startsWith(startString))
-    .map((item) => item.slice(startString.length))
-    .join('-');
+  if (!startString || typeof startString !== 'string' || startString === '') {
+    return '';
+  }
 
-  return newString;
+  const result = [];
+
+  for (const item of set) if (item.startsWith(startString)) result.push(item.slice(startString.length));
+
+  return result.join('-');
 }
