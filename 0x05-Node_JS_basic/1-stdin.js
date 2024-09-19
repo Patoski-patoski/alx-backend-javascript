841a3adb9fe61bbf7fd2863a066a4e3ea981bea7
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-19 04:59:28 +0000

    a program named 1-stdin.js that will be executed through command line:

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
-console.log(`Welcome to Holberton School, what is your name?`)
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim()
-    console.log(`Your name is: ${name}`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-    process.exit();
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
 });
-process.on('exit', () => {
-    console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
