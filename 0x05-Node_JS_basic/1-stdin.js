a7aa43bb3753594dfa32caf02c01b6dad46fe846
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-21 03:40:04 +0000

    using express

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
 
-process.stdin.on('readable', () => {
-  const data = process.stdin.read();
-  if (data) {
-    process.stdout.write(`Your name is: ${data}`);
-  }
+  process.exit();
 });
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
