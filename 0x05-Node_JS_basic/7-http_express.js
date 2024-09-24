0fd6561d9e697c9ecc90b957a08e80cbc59f0e4c
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-24 04:30:25 +0000

    recreate the small HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+const PORT = 1245;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const rows = data.split('\n').filter((row) => row.trim() !== '');
+    const headers = rows[0].split(',');
+
+    let num = 0;
+    let CS = 0;
+    let SWE = 0;
+    const CSArray = [];
+    const SWEArray = [];
+
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i].split(',');
+      const rowData = {};
+
+      headers.forEach((header, index) => {
+        rowData[header.trim()] = row[index];
+      });
+      num += 1;
+
+      if (rowData.field === 'CS') {
+        CS += 1;
+        CSArray.push(rowData.firstname);
+      } else if (rowData.field === 'SWE') {
+        SWE += 1;
+        SWEArray.push(rowData.firstname);
+      }
+    }
+    let numOfStudents = `Number of students: ${num}\n`;
+    numOfStudents += (`Number of students in CS: ${CS}. List: ${CSArray.join(', ')}\n`);
+    numOfStudents += (`Number of students in SWE: ${SWE}. List: ${SWEArray.join(', ')}\n`);
+
+    return numOfStudents.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/students', async (req, res) => {
+  const databaseName = process.argv[2] || 'database.csv';
+  try {
+    const data = await countStudents(databaseName);
+    res.send(`This is the list of our students\n${data}`);
+  } catch (error) {
+    res.statusCode = 404;
+    res.end(error.message);
+  }
+});
+app.listen(PORT, () => {
+  console.log(`Server => ${PORT}`);
+});
