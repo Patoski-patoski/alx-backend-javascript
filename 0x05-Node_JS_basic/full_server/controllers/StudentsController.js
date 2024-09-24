c1f831a577489520a91c9db890bff34dacf5f366
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-24 11:21:54 +0000

    created fullserver, AppController, StudentController

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,34 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      const data = await readDatabase('database.csv');
+      return response.status(200).send(`This is the list of our students\n${data.join('').trim()}`);
+    } catch (error) {
+      return response.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    const databaseName = process.argv[2] || 'database.csv';
+    let responseData;
+    try {
+      const param = request.params.major.toString().trim();
+      if (typeof (param) !== 'string' || (param !== 'SWE' && param !== 'CS')) {
+        responseData = response.status(500).send('Major parameter must be CS or SWE');
+      }
+      const data = await readDatabase(databaseName);
+      if (param === 'CS') {
+        responseData = response.status(200).send(data[1].trim());
+      } else if (param === 'SWE') {
+        responseData = response.status(200).send(data[2].trim());
+      }
+      return responseData;
+    } catch (error) {
+      return response.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,42 @@
+import { promises as fs } from 'fs';
+
+async function readDatabase(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const rows = data.split('\n').filter((row) => row.trim() !== '');
+    const headers = rows[0].split(',');
+
+    let num = 0;
+    let CS = 0;
+    let SWE = 0;
+    const CSArray = [];
+    const SWEArray = [];
+
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i].split(',');
+      const rowData = {};
+
+      headers.forEach((header, index) => {
+        rowData[header.trim()] = row[index];
+      });
+      num += 1;
+
+      if (rowData.field === 'CS') {
+        CS += 1;
+        CSArray.push(rowData.firstname);
+      } else if (rowData.field === 'SWE') {
+        SWE += 1;
+        SWEArray.push(rowData.firstname);
+      }
+    }
+    const arrnumOfStudents = [];
+    arrnumOfStudents[0] = `Number of students: ${num}\n`;
+    arrnumOfStudents[1] = (`Number of students in CS: ${CS}. List: ${CSArray.join(', ')}\n`);
+    arrnumOfStudents[2] = (`Number of students in SWE: ${SWE}. List: ${SWEArray.join(', ')}\n`);
+
+    return arrnumOfStudents;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+export default readDatabase;
